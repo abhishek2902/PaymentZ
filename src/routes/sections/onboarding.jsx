3ee29c6734f97b376ed39400dae06aab45f1f21d
Harry <lalitgupta2902@gmail.com>
2026-01-13 16:15:36 +0530

    onboarding fixed

--- src/routes/sections/onboarding.jsx
+++ src/routes/sections/onboarding.jsx
@@ -1,13 +1,43 @@
-import ThankYouPage from 'src/components/onboarding/ThankYouPage';
-import OnboardingForm from 'src/pages/onboard/onboarding-form';
+// import ThankYouPage from 'src/components/onboarding/ThankYouPage';
+// import OnboardingForm from 'src/pages/onboard/onboarding-form';
+
+// export const onboardingRoutes = [
+//   {
+//     path: '/admin/onboarding/:onboardingId',
+//     element: <OnboardingForm />,
+//   },
+//   {
+//     path: '/admin/onboarding/thank-you',
+//     element: <ThankYouPage />,
+//   },
+// ];
+
+import { lazy, Suspense } from 'react';
+import { SplashScreen } from 'src/components/loading-screen';
+
+const OnboardingForm = lazy(() =>
+  import('src/pages/onboard/onboarding-form')
+);
+
+const ThankYouPage = lazy(() =>
+  import('src/components/onboarding/ThankYouPage')
+);
 
 export const onboardingRoutes = [
   {
     path: '/admin/onboarding/:onboardingId',
-    element: <OnboardingForm />,
+    element: (
+      <Suspense fallback={<SplashScreen />}>
+        <OnboardingForm />
+      </Suspense>
+    ),
   },
   {
     path: '/admin/onboarding/thank-you',
-    element: <ThankYouPage />,
+    element: (
+      <Suspense fallback={<SplashScreen />}>
+        <ThankYouPage />
+      </Suspense>
+    ),
   },
 ];
